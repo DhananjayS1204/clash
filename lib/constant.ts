6359420c65565e522efd8dabcab87154c58bf9e5
DhananjayS1204 <147143159+DhananjayS1204@users.noreply.github.com>
2024-07-21 15:22:38 +0530

    Update constant.ts

--- lib/constant.ts
+++ lib/constant.ts
@@ -9,6 +9,97 @@ export const carddata = [
         image: '/keys.jpeg',
         description: ''    
     },
+     {
+        name: 'Book',
+        place: 'indore',
+        type: 'lost',
+        date: '10 july',
+        image: '/Book.jpeg',
+        description: ''    
+    },
+     {
+        name: 'Earbuds',
+        place: 'indore',
+        type: 'lost',
+        date: '11 july',
+        image: '/Earbuds.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Earring',
+        place: 'indore',
+        type: 'lost',
+        date: '15 july',
+        image: '/Earring.jpeg',
+        description: ''    
+    },
+    {
+        name: 'pen',
+        place: 'indore',
+        type: 'lost',
+        date: '20 july',
+        image: '/pen.jpeg',
+        description: ''    
+    },
+    {
+        name: 'phone',
+        place: 'indore',
+        type: 'lost',
+        date: '25 july',
+        image: '/phone.jpeg',
+        description: ''    
+    },
+   
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    {
+        name: 'Keys',
+        place: 'indore',
+        type: 'found',
+        date: '7 Nov',
+        image: '/keys.jpeg',
+        description: ''    
+    },
+    
+    
     {
 
     }
